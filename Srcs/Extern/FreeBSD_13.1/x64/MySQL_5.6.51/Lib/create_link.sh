3cb2218ad5726cb0e9de25c0a44ec1c75c8df7bb
Sercan Berk GÜLER <officialberqq@gmail.com>
2023-01-07 08:10:37 +0300

    New Tree Styling.

---
+++ Srcs/Extern/FreeBSD_13.1/x64/MySQL_5.6.51/Lib/create_link.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -s   libmysqlclient.so.18    libmysqlclient.so
+ln -s   libmysqlclient.a        libmysqlclient_r.a
+ln -s   libmysqlclient.so       libmysqlclient_r.so
+ln -s   libmysqlclient.so.18    libmysqlclient_r.so.18
